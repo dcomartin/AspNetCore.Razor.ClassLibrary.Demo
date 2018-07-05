081bc4848acb0b6219d5cb8343787092473a719e
Derek Comartin <dcomartin@gmail.com>
2018-07-05 14:19:45 -0400

    Demo

---
+++ Web/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web
+{
+    public class DemoController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View("/MyView.cshtml");
+        }
+    }
+}
---
+++ Web/Startup.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: AspMvcViewLocationFormat(@"~/../ClassLibrary")]
+
+namespace Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
